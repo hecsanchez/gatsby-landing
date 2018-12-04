beaa33de5940c040674838147d9f54f9e3e4fa74
Hector Sanchez <hola@hectorsanchez.mx>
2018-12-03 20:08:05 -0600

    Adds Section component and basic layout for Index

--- src/containers/section/Section.js
+++ src/containers/section/Section.js
@@ -1,17 +1,17 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import './section.scss'
 
-const Section = () => (
+const Section = ({ title, subtitle, children }) => (
   <div>
+    <h1>{ title }</h1>
+    <h1>{ subtitle }</h1>
+    { children }
   </div>
 )
 
 Section.propTypes = {
-}
-
-Section.defaultProps = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Section
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
+import PricingTable from '../components/pricingTable/pricingTable'
 import Layout from '../containers/layout/layout'
-import Image from '../components/image'
+import Section from '../containers/section/section'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Section 
+      title={'The Best Way to Work Together'} 
+      subtitle={'We design and develop software for funded startups and established companies. Here are just some examples of our work'}
+      background={'#ffffff'}
+      >
+      
+    </Section>
+    <Section 
+      title={'Simple Pricing'} 
+      subtitle={'It seems from the moment you begin to take your love of astronomy seriously, the thing that is on your mind is what kind.'}
+      background={'#F9F9F9'}
+      >
+      <PricingTable>
+        
+      </PricingTable>
+    </Section>
+    <Section 
+      title={'The Ultimate Pasta Length Editor'} 
+      subtitle={'How to boost the design and developing process with a little help of four paws. True story by Snoop Dogg.'}
+      background={'#5E6977'}
+      >
+      
+    </Section>
   </Layout>
 )
 
